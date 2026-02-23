3d4a40c21c180511bc920e77110cf4a6d9790290
sadabehtesham <csds23083@glbitm.ac.in>
2026-02-23 11:06:14 +0530

    Time: 175 ms (40.00%) | Memory: 96.8 MB (43.21%) - LeetSync

---
+++ 1557-check-if-a-string-contains-all-binary-codes-of-size-k/check-if-a-string-contains-all-binary-codes-of-size-k.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean hasAllCodes(String s, int k) {
+         if (s.length() < k) return false;
+
+        Set<String> seen = new HashSet<>();
+
+        for (int i = 0; i <= s.length() - k; i++) {
+            seen.add(s.substring(i, i + k));
+        }
+
+        return seen.size() == (1 << k);
+    }
+}
